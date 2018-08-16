1861af639144ff8ccc2984318c885e1aaac7c472
Hans Goos <gooshans26@gmail.com>
2018-08-16 20:06:24 +0200

    Adjust background color on navbar resizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
-// adjust color navbar on scrolling
-
 var navbar = document.querySelector(".navbar");
 
+// adjust color navbar on scrolling
+
 window.addEventListener("scroll", function() {
-    if(window.scrollY < 100) {
+    if(window.scrollY < 100 || window.scrollX > 720) {
+        navbar.style.backgroundColor = "transparent";
+    } else {
+        navbar.style.backgroundColor = "white";
+    }
+});
+
+// adjust color navbar on resizing
+
+window.addEventListener("resize", function () {
+    console.log(document.body.clientWidth);
+    if (document.body.clientWidth >= 992) {
         navbar.style.backgroundColor = "transparent";
     } else {
         navbar.style.backgroundColor = "white";
